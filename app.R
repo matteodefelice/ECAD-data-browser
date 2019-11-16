c7045abed407b2e0355dc4ea1f61c510559ca165
Matteo De Felice <matteo.defelice@gmail.com>
2019-11-16 22:27:52 +0100

    Switched to dygraphs. Added labels and updated text

--- app.R
+++ app.R
@@ -1,7 +1,25 @@
 library(shiny)
 library(leaflet)
 library(tidyverse)
-library(highcharter)
+library(xts)
+library(dygraphs)
+
+units = list(
+  CC = list(name = 'Cloud cover', unit = 'Oktas', scale = 1),
+  WD = list(name = 'Wind Direction', unit = 'Degrees', scale = 1),
+  FG = list(name = 'Wind Speed', unit = 'Speed (m/s)', scale = 10),
+  FX = list(name = 'Wind Gust', unit = 'Speed (m/s)', scale = 10),
+  HU = list(name = 'Humidity', unit = '%', scale = 1),
+  PP = list(name = 'Pressure', unit = 'hPa', scale = 10),
+  RR = list(name = 'Precipitation', unit = 'Millimeters (mm)', scale = 10),
+  SD = list(name = 'Snow Depth', unit = 'Centimeters (cm)', scale = 1),
+  SS = list(name = 'Sunshine', unit = 'Hours', scale = 10),
+  TG = list(name = 'Mean Temperature', unit = 'Celsius Degrees (C)', scale = 10),
+  TN = list(name = 'Min Temperature', unit = 'Celsius Degrees (C)', scale = 10),
+  TX = list(name = 'Max Temperature', unit = 'Celsius Degrees (C)', scale = 10)
+)
+  
+
 read_station_data <- function(filename) {
   nskips = 20
   if (str_sub(filename, 1,2) == 'RR') {
@@ -14,65 +32,64 @@ read_station_data <- function(filename) {
                                         DATE = col_date(format = "%Y%m%d"))) %>% 
     select(-starts_with('Q_')) %>%
     gather(variable, value, -STAID, -SOUID, -DATE) %>%
-    filter(value > -9999)
+    dplyr::filter(value > -9999)
   return(stn_data)
 }
 
 ### USER INTERFACE ------------------------------------------------------
 ui <- fluidPage(
-  titlePanel("European Climate Assessment & Dataset (ECA&D) stations metadata browser"),
+  titlePanel("European Climate Assessment & Dataset (ECA&D) stations data browser"),
   tags$head(
     # Include custom CSS
     includeCSS("styles.css")
   ),
-
+  
   fluidRow(
     column(
       3,
-      p("Browse the available stations in the Blended ECA Dataset. Click on each marker to see how to access the data."),
+      p("Browse the available stations in the Blended ECA Dataset. Click on each marker to see the data and the metadata."),
       wellPanel(
         h4("Filter"),
         # Select the minimum number of years
         sliderInput("years", "Minimum number of years",
-          0, 80, 10,
-          step = 10
+                    0, 80, 10,
+                    step = 10
         ),
         # choose betwee the 'any' and the 'all' condition
         radioButtons("any_all",
-          label = "Filter all the stations with",
-          choices = c("any", "all"), inline = TRUE
+                     label = "Filter all the stations with",
+                     choices = c("any", "all"), inline = TRUE
         ),
         # Choose the variables
         checkboxGroupInput("elems",
-          label = "the following elements:",
-          choices = c(
-            "Cloud cover (CC)" = "CC",
-            "Wind Direction (DD)" = "DD",
-            "Wind Speed (FG)" = "FG",
-            "Wind Gust (FX)" = "FX",
-            "Humidity (HU)" = "HU",
-            "Pressure (PP)" = "PP",
-            "Precipitation (RR)" = "RR",
-            "Snow Depth (SD)" = "SD",
-            "Sunshine (SS)" = "SS",
-            "Mean Temperature (TG)" = "TG",
-            "Min Temperature (TN)" = "TN",
-            "Max Temperature (TX)" = "TX"
-          ),
-          selected = "TG"
+                           label = "the following elements:",
+                           choices = c(
+                             "Cloud cover (CC)" = "CC",
+                             "Wind Direction (DD)" = "DD",
+                             "Wind Speed (FG)" = "FG",
+                             "Wind Gust (FX)" = "FX",
+                             "Humidity (HU)" = "HU",
+                             "Pressure (PP)" = "PP",
+                             "Precipitation (RR)" = "RR",
+                             "Snow Depth (SD)" = "SD",
+                             "Sunshine (SS)" = "SS",
+                             "Mean Temperature (TG)" = "TG",
+                             "Min Temperature (TN)" = "TN",
+                             "Max Temperature (TX)" = "TX"
+                           ),
+                           selected = "TG"
         ),
         actionButton("selectall", label = "Select all")
       )
     ),
     column(
       9,
-      leafletOutput("stations_map", height = "600px"),
-      highchartOutput("hcontainer", height = "600px"),
-      # textOutput('temp'),
+      leafletOutput("stations_map", height = "500px"),
+      dygraphOutput("dyts", height = "400px"),
       wellPanel(
         tags$p(paste0(
           "Data from the European Climate Assessment & Dataset project. ",
-          "Updated August 2018. "
+          "Updated November 2019. "
         ), a("Data description & policy", href = "https://www.ecad.eu//dailydata/index.php")),
         tags$p("Developed by ", 
                a("Matteo De Felice", href = "http://matteodefelice.name"),
@@ -82,7 +99,7 @@ ui <- fluidPage(
                "with",
                a("CC BY 4.0", href = "https://creativecommons.org/licenses/by/4.0/"),
                "license"
-               )
+        )
       )
     )
   )
@@ -115,15 +132,15 @@ server <- function(input, output, session) {
         layerId = ~filename,
         ~lon_dec, ~lat_dec,
         popup = ~paste0("<h4>", STANAME , " (", CN, ")</h2><br/>",
-                       "Station id: ", STAID, "<br/>",
-                       "Station height: ", HGHT, "<br/>",
-                       "Variable: ", base_ELEI, "<br/>",
-                       "Number of sources used for blending: ", n_sources, "<br/>",
-                       "Time-series range from ", START, " to ", STOP, "<br/>",
-                       "Filename: <code>", filename, "</code>",
-                       ' available downloading <code>ECA_blended_', tolower(base_ELEI),
-                       '.zip</code> from <a href="https://www.ecad.eu/dailydata/predefinedseries.php">this page</a><br/>'
-            ),
+                        "Station id: ", STAID, "<br/>",
+                        "Station height: ", HGHT, "<br/>",
+                        "Variable: ", base_ELEI, "<br/>",
+                        "Number of sources used for blending: ", n_sources, "<br/>",
+                        "Time-series range from ", START, " to ", STOP, "<br/>",
+                        "Filename: <code>", filename, "</code>",
+                        ' available downloading <code>ECA_blended_', tolower(base_ELEI),
+                        '.zip</code> from <a href="https://www.ecad.eu/dailydata/predefinedseries.php">this page</a><br/>'
+        ),
         popupOptions = popupOptions(textsize = "15px", opacity = 0.7),
         label = ~as.character(paste(STANAME, '(', STAID, ')', 
                                     base_ELEI)),
@@ -132,33 +149,37 @@ server <- function(input, output, session) {
       ) 
   })
   #Time-series
-  output$hcontainer <- renderHighchart({
+  
+  output$dyts <- renderDygraph({
     if (!is.null(input$stations_map_marker_click)) {
-      toplot = read_station_data(paste0('data/', 
+      toplot = read_station_data(paste0('data/',
                                         input$stations_map_marker_click$id)) %>%
         select(date = DATE, value) 
       
+      # Load unit measure
+      this_unit = units[[str_sub(input$stations_map_marker_click$id, 1, 2)]]
+      # Scale value
+      toplot$value = toplot$value / this_unit$scale
+      # Get name
+      sel_stn = eobs_processed %>%
+        dplyr::filter(filename == input$stations_map_marker_click$id) %>%
+        select(STANAME, CN)
+      # Create XTS
       toplot_xts = xts::xts(x = toplot$value, order.by = toplot$date)
-      # hc = hchart(toplot, 'line', hcaes(x = DATE, y = value),
-      #             range = 10
-      #             ) %>%
-      #   hc_chart(zoomType = 'x')
-      hc = highchart(type = "stock") %>%
-        hc_add_series(data = toplot_xts,type = 'line')
       
-      hc
+      dy = dygraph(toplot_xts,
+                   main = paste0(this_unit$name, ' - ', sel_stn$STANAME, ' (', sel_stn$CN, ')'),
+                   ylab = this_unit$unit
+                   ) %>% 
+        dyRangeSelector(dateWindow = (toplot %>% tail(365) %>% pull(date))[c(1,365)]
+                        ) %>%
+        dyRoller() %>%
+        dyLimit(quantile(toplot$value, 0.05, na.rm = TRUE) %>% as.numeric(), color = 'blue') %>%
+        dyLimit(quantile(toplot$value, 0.95, na.rm = TRUE) %>% as.numeric(), color = 'red')
+      dy
     }
-    
-    
   })
-
   
-  # output$temp <- renderText({
-  #   if (!is.null(input$stations_map_marker_click)) {
-  #     
-  #     input$stations_map_marker_click$id
-  #   }
-  # })
   # Observe part
   observe({
     
