d1eaa049e4c0c8e9d1638b502263101a4d68b0e5
Matteo De Felice <matteo.defelice@gmail.com>
2019-11-16 21:48:38 +0100

    First implementation of highchart.

--- app.R
+++ app.R
@@ -1,6 +1,22 @@
 library(shiny)
 library(leaflet)
 library(tidyverse)
+library(highcharter)
+read_station_data <- function(filename) {
+  nskips = 20
+  if (str_sub(filename, 1,2) == 'RR') {
+    nskips = 21
+  }
+  
+  stn_data <- read_csv(filename, skip = nskips, 
+                       col_types = cols(STAID = col_character(),
+                                        SOUID = col_character(),
+                                        DATE = col_date(format = "%Y%m%d"))) %>% 
+    select(-starts_with('Q_')) %>%
+    gather(variable, value, -STAID, -SOUID, -DATE) %>%
+    filter(value > -9999)
+  return(stn_data)
+}
 
 ### USER INTERFACE ------------------------------------------------------
 ui <- fluidPage(
@@ -51,6 +67,8 @@ ui <- fluidPage(
     column(
       9,
       leafletOutput("stations_map", height = "600px"),
+      highchartOutput("hcontainer", height = "600px"),
+      # textOutput('temp'),
       wellPanel(
         tags$p(paste0(
           "Data from the European Climate Assessment & Dataset project. ",
@@ -94,6 +112,7 @@ server <- function(input, output, session) {
       setView(lng = 10, lat = 50, zoom = 4) %>%
       addMarkers(
         data = selected_stations(),
+        layerId = ~filename,
         ~lon_dec, ~lat_dec,
         popup = ~paste0("<h4>", STANAME , " (", CN, ")</h2><br/>",
                        "Station id: ", STAID, "<br/>",
@@ -102,7 +121,7 @@ server <- function(input, output, session) {
                        "Number of sources used for blending: ", n_sources, "<br/>",
                        "Time-series range from ", START, " to ", STOP, "<br/>",
                        "Filename: <code>", filename, "</code>",
-                       ' available downloading <code>ECA_blended_', tolower(base_ELEI), 
+                       ' available downloading <code>ECA_blended_', tolower(base_ELEI),
                        '.zip</code> from <a href="https://www.ecad.eu/dailydata/predefinedseries.php">this page</a><br/>'
             ),
         popupOptions = popupOptions(textsize = "15px", opacity = 0.7),
@@ -110,9 +129,39 @@ server <- function(input, output, session) {
                                     base_ELEI)),
         labelOptions = labelOptions(textsize = "15px", opacity = 0.7),
         clusterOptions = markerClusterOptions()
-      )
+      ) 
   })
+  #Time-series
+  output$hcontainer <- renderHighchart({
+    if (!is.null(input$stations_map_marker_click)) {
+      toplot = read_station_data(paste0('data/', 
+                                        input$stations_map_marker_click$id)) %>%
+        select(date = DATE, value) 
+      
+      toplot_xts = xts::xts(x = toplot$value, order.by = toplot$date)
+      # hc = hchart(toplot, 'line', hcaes(x = DATE, y = value),
+      #             range = 10
+      #             ) %>%
+      #   hc_chart(zoomType = 'x')
+      hc = highchart(type = "stock") %>%
+        hc_add_series(data = toplot_xts,type = 'line')
+      
+      hc
+    }
+    
+    
+  })
+
+  
+  # output$temp <- renderText({
+  #   if (!is.null(input$stations_map_marker_click)) {
+  #     
+  #     input$stations_map_marker_click$id
+  #   }
+  # })
+  # Observe part
   observe({
+    
     if (input$selectall > 0) {
       opts <- list(
         "Cloud cover (CC)" = "CC",
@@ -136,6 +185,7 @@ server <- function(input, output, session) {
       )
     }
   })
+  
 }
 
 # Run the application
